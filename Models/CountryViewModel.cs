7b85d630198e2bf5cc7269011c1f6ff9aabe39d7
Buyani Mhlongo <buyanimhlongo@gmail.com>
2022-04-13 17:24:30 +0200

    model update

---
+++ Models/CountryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace palota_func_countries_assessment.Models
+{
+    public class CountryViewModel
+    {
+        public string name { get; set; }
+        public string iso3Code { get; set; }
+        public string capital { get; set; }
+        public string subregion { get; set; }
+        public string region { get; set; }
+        public int population { get; set; }
+        public Location location { get; set; }
+        public string demonym { get; set; }
+        public string nativeName { get; set; }
+        public string numericCode { get; set; }
+        public string flag { get; set; }
+    }
+}
